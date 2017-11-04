584ae6b9c115492fad79b0d111b6e84c7847c6b9
AdamRomyn <adamromyn1235@gmail.com>
2017-11-04 06:40:22 +0200

    added breakpoint maxin and values for breakpoints, fixed up the first slanted white skew-line

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-
+//Adds the correct class to the body tag based on the operating system
 var add_os_class_to_body = function(){
   var OSName = "Unknown";
   if (window.navigator.userAgent.indexOf("Windows NT 10.0")!= -1) OSName="windows";
@@ -12,11 +12,18 @@ var add_os_class_to_body = function(){
   if (window.navigator.userAgent.indexOf("Mac")            != -1) OSName="ios";
   if (window.navigator.userAgent.indexOf("X11")            != -1) OSName="unix";
   if (window.navigator.userAgent.indexOf("Linux")          != -1) OSName="linux";
-  console.log(OSName);
+
   $('body').addClass(OSName);
 }
 
+//Gets the year and inserts it into any element with the class year
+var insert_current_year = function(){
+  var year = (new Date()).getFullYear();
+  document.getElementById("year").innerHTML = year;
+}
 
+//execute functions
+insert_current_year();
 add_os_class_to_body();
 
 });
