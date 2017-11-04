6847822877a7f63300104235dae93bdbac60bb84
AdamRomyn <adamromyn1235@gmail.com>
2017-11-04 09:12:22 +0200

    Added some animations and made the site responsive

--- js/index.js
+++ js/index.js
@@ -16,14 +16,71 @@ var add_os_class_to_body = function(){
   $('body').addClass(OSName);
 }
 
+function isVisible(elem) {
+    if (!(elem instanceof Element)) throw Error('DomUtil: elem is not an element.');
+    const style = getComputedStyle(elem);
+    if (style.display === 'none') return false;
+    if (style.visibility !== 'visible') return false;
+    if (style.opacity < 0.1) return false;
+    if (elem.offsetWidth + elem.offsetHeight + elem.getBoundingClientRect().height +
+        elem.getBoundingClientRect().width === 0) {
+        return false;
+    }
+    const elemCenter   = {
+        x: elem.getBoundingClientRect().left + elem.offsetWidth / 2,
+        y: elem.getBoundingClientRect().top + elem.offsetHeight / 2
+    };
+    if (elemCenter.x < 0) return false;
+    if (elemCenter.x > (document.documentElement.clientWidth || window.innerWidth)) return false;
+    if (elemCenter.y < 0) return false;
+    if (elemCenter.y > (document.documentElement.clientHeight || window.innerHeight)) return false;
+    let pointContainer = document.elementFromPoint(elemCenter.x, elemCenter.y);
+    do {
+        if (pointContainer === elem) return true;
+    } while (pointContainer = pointContainer.parentNode);
+    return false;
+}
+
 //Gets the year and inserts it into any element with the class year
 var insert_current_year = function(){
   var year = (new Date()).getFullYear();
   document.getElementById("year").innerHTML = year;
 }
 
+var aboutAnimated = false;
+var updatesAnimated = false;
+
+var check_animations = function(){
+  //console.log("top: " + $(document).scrollTop());
+  //console.log("udpate block: " + $('.updates').offset().top);
+  if(isVisible('.about',600) && aboutAnimated == false){
+    aboutAnimated = true;
+    $('.about-inner').addClass("bounceInRight animated");
+    console.log("asdf");
+  }
+  if(isVisible('.updates') && updatesAnimated == false){
+    updatesAnimated = true;
+    $('.update-block').addClass("fadeInTop animated");
+    console.log("awe");
+  }
+}
+
+var isVisible = function(elem, threshold){
+  if($(document).scrollTop() > $(elem).offset().top - threshold){
+    return true;
+  }else{
+    return false;
+  }
+}
+
 //execute functions
 insert_current_year();
 add_os_class_to_body();
+
+//Check for scrolling and animate if needed
+check_animations();
+$(document).on('scroll',function(){
+  check_animations();
+})
 
 });
