595e475e0c2b6ac409607ff69f828e24cf292e2a
Adam Romyn <adamromyn12345@gmail.com>
2017-11-04 18:40:58 +0200

    everything done except animations and menu responsive expansion

--- js/index.js
+++ js/index.js
@@ -16,30 +16,6 @@ var add_os_class_to_body = function(){
   $('body').addClass(OSName);
 }
 
-function isVisible(elem) {
-    if (!(elem instanceof Element)) throw Error('DomUtil: elem is not an element.');
-    const style = getComputedStyle(elem);
-    if (style.display === 'none') return false;
-    if (style.visibility !== 'visible') return false;
-    if (style.opacity < 0.1) return false;
-    if (elem.offsetWidth + elem.offsetHeight + elem.getBoundingClientRect().height +
-        elem.getBoundingClientRect().width === 0) {
-        return false;
-    }
-    const elemCenter   = {
-        x: elem.getBoundingClientRect().left + elem.offsetWidth / 2,
-        y: elem.getBoundingClientRect().top + elem.offsetHeight / 2
-    };
-    if (elemCenter.x < 0) return false;
-    if (elemCenter.x > (document.documentElement.clientWidth || window.innerWidth)) return false;
-    if (elemCenter.y < 0) return false;
-    if (elemCenter.y > (document.documentElement.clientHeight || window.innerHeight)) return false;
-    let pointContainer = document.elementFromPoint(elemCenter.x, elemCenter.y);
-    do {
-        if (pointContainer === elem) return true;
-    } while (pointContainer = pointContainer.parentNode);
-    return false;
-}
 
 //Gets the year and inserts it into any element with the class year
 var insert_current_year = function(){
@@ -47,24 +23,7 @@ var insert_current_year = function(){
   document.getElementById("year").innerHTML = year;
 }
 
-var aboutAnimated = false;
-var updatesAnimated = false;
-
-var check_animations = function(){
-  //console.log("top: " + $(document).scrollTop());
-  //console.log("udpate block: " + $('.updates').offset().top);
-  if(isVisible('.about',600) && aboutAnimated == false){
-    aboutAnimated = true;
-    $('.about-inner').addClass("bounceInRight animated");
-    console.log("asdf");
-  }
-  if(isVisible('.updates') && updatesAnimated == false){
-    updatesAnimated = true;
-    $('.update-block').addClass("fadeInTop animated");
-    console.log("awe");
-  }
-}
-
+//Checks if an element is currently visible to the user
 var isVisible = function(elem, threshold){
   if($(document).scrollTop() > $(elem).offset().top - threshold){
     return true;
@@ -72,6 +31,28 @@ var isVisible = function(elem, threshold){
     return false;
   }
 }
+
+
+//Animation
+var aboutAnimated = false;
+var updatesAnimated = false;
+
+//Checks to see if any of the sections are visible and therefore causes and animation
+var check_animations = function(){
+  console.log("top: " + $(document).scrollTop());
+  console.log("udpate block: " + $('.updates').offset().top);
+  if(isVisible('.about',600) && aboutAnimated == false){
+    aboutAnimated = true;
+    $('.about-inner').addClass("bounceInRight animated");
+    console.log("asdf");
+  }
+  if(isVisible('.updates',0) && updatesAnimated == false){
+    updatesAnimated = true;
+    $('.update-block').addClass("fadeInTop animated");
+    console.log("awe");
+  }
+}
+
 
 //execute functions
 insert_current_year();
@@ -82,5 +63,7 @@ check_animations();
 $(document).on('scroll',function(){
   check_animations();
 })
+
+
 
 });
